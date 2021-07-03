a91762697dc9b017d14d8f37a87c27fbacdb31e7
mskaushan <mskaushan@yandex.ru>
2021-07-03 05:16:14 +0300

    Сhanged the script for closing the menu and the scrollbar

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
-[].forEach.call(document.querySelectorAll('.menu-item'), function (el){
-    el.addEventListener('click', function(e){
+/*
+[].forEach.call(document.querySelectorAll('.menu-item'), function (el) {
+    el.addEventListener('click', function(e) {
         document.querySelector('input[type="checkbox"].input').checked = false;
     });
 });
+*/
+
+/*
+document.querySelectorAll('.menu-item').forEach((item) =>
+    item.addEventListener('click', () => {
+        document.querySelector('input[type="checkbox"].input').checked = false;
+    })
+)
+*/
+
+document.querySelector('.menu').addEventListener('click', () => {
+    document.querySelector('input[type="checkbox"].input').checked = false;
+})
